9be6d6ce4f8b3b270c219b4dd74bbd0032df5325
SettleACG <settleacg@gmail.com>
2019-08-30 14:40:57 +0800

    Start work Activiti

---
+++ Activiti_Demo_2/src/main/java/com/amaduse/generate/GenerateApplication.java
@@ -0,0 +1,7 @@
+package com.amaduse.generate;
+
+import org.springframework.boot.SpringBootConfiguration;
+
+@SpringBootConfiguration
+public class GenerateApplication {
+}
---
+++ Activiti_Demo_2/src/main/java/com/amaduse/service/TestService.java
@@ -0,0 +1,4 @@
+package com.amaduse.service;
+
+public interface TestService {
+}
